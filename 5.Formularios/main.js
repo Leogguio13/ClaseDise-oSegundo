45b3d2bd32950e56a36e0a417c17de88124f5711
Leogguio13 <jolegogui@gmail.com>
2023-02-03 00:54:00 -0500

    primera version

---
+++ 5.Formularios/main.js
@@ -0,0 +1,13 @@
+"use strict";
+//variables o contenedores 
+
+var numero1 = 13; //global
+let numero2 = 25; //local
+const IVA = 19.1;
+
+if(true){
+    let numero2 = 50;
+    console.log(numero2);
+}
+
+console.log(numero2);
