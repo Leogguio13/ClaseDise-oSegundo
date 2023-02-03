d80bb4aaf34e0a853066c5e99b072cf14059c36b
Leogguio13 <jolegogui@gmail.com>
2023-02-03 02:24:21 -0500

    tercera version

--- 5.Formularios/main.js
+++ 5.Formularios/main.js
@@ -1,5 +1,26 @@
 "use strict";
-//variables o contenedores 
+let boton = document.getElementById("enviar");
+const formulario = document.getElementById("form");
+let myH1 = document.getElementsByTagName("h1");
+console.log(myH1);
+
+/*boton.addEventListener("click", (e)=>{
+    e.preventDefault();
+    console.log(formulario[0].value);
+    console.log(formulario[1].value);
+    console.log(formulario[2].value);
+    console.log(formulario[3].value);
+    console.log(formulario[5].value);
+});*/
+
+/*formulario.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    console.log(e.target[1].value);
+});*/
+
+//console.log(formulario);
+
+/*variables o contenedores 
 
 var numero1 = 13; //global
 let numero2 = 25; //local
@@ -11,4 +32,4 @@ if(true){
 }
 
 console.log(numero2);
-console.log(numero2);
+console.log(numero2);*/
